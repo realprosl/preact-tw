9b790a596cc4cdb2def2323fdc4b156f49db22ec
proDev1981 <a.p.g.valencia@gmail.com>
2024-05-01 16:18:41 +0200

    add texfield component

--- src/app.tsx
+++ src/app.tsx
@@ -1,19 +1,12 @@
-import { Signal, signal } from "@preact/signals";
-
-const count = signal(0);
+import { TextField } from "./components/textfield";
 
 export function App() {
   return (
-    <div className="flex flex-col">
-      <span className={isNegative(count)}>{count}</span>
-      <div className="flex flex-row space-x-2">
-        <button onClick={() => count.value++}>add</button>
-        <button onClick={() => count.value--}>minus</button>
+    <>
+      <div className="flex flex-col gap-2 mt-2 ml-2">
+        <TextField label="Name" />
+        <TextField label="SubName" />
       </div>
-    </div>
+    </>
   );
-}
-
-function isNegative(count: Signal<number>): string {
-  return count.value < 0 ? "text-red-400" : "text-green-400";
 }
