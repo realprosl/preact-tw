60e7eff7e525d6db1b60efd1a4a4a9f9dea661fe
proDev1981 <a.p.g.valencia@gmail.com>
2024-05-04 18:10:32 +0200

    add animated-tailwindcss midudev

--- src/app.tsx
+++ src/app.tsx
@@ -3,7 +3,7 @@ import { TextField } from "./components/textfield";
 export function App() {
   return (
     <>
-      <div className="flex flex-col gap-2 mt-2 ml-2">
+      <div className="animate-fade-in flex flex-col gap-2 mt-2 ml-2">
         <TextField label="Name" />
         <TextField label="SubName" />
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
+import animations from "@midudev/tailwind-animations"
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{ts,js,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [animations],
 }
 
