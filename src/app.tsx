9366166489337f7d44bff14e4fd83d5d307a1b8b
proDev1981 <a.p.g.valencia@gmail.com>
2024-05-05 16:42:31 +0200

    refactor width input of realsearch

--- src/app.tsx
+++ src/app.tsx
@@ -6,7 +6,7 @@ export function App() {
     <>
       <div className="flex flex-col gap-2 mt-2 ml-2">
         <TextField label="Name" />
-        <RealSearch size="96%" placeholder=" Search" />
+        <RealSearch size="96%" placeholder=" Search.." />
       </div>
     </>
   );
--- src/components/realSearch.tsx
+++ src/components/realSearch.tsx
@@ -12,12 +12,12 @@ export const RealSearch = (props: Props) => {
     <div className={`w-[${props.size || "90%"}]`}>
       <div className="overflow-hidden relative h-[32px] min-w-44 rounded-xl shadow-md has-[:hover]:shadow-slate-400">
         <input
-          className="absolute bg-transparent top-[4px] left-9 rounded-md peer outline-none w-[96%]"
+          className="absolute bg-transparent top-[4px] left-9 rounded-md peer outline-none max-w-[96%]"
           type="text"
           ref={text} 
           placeholder={props.placeholder}
         />
-        <div className="absolute top-0 -z-10 h-[32px] border w-[100%] border-gray-400 peer-hover:border-none shadow-sm rounded-xl"></div>
+        <div className="absolute top-0 -z-10 h-[32px] border w-[100%] border-gray-400 peer-hover:border-none shadow-sm rounded-full"></div>
         <div className="absolute left-1 top-[4px]  rounded-xl bg-black w-6 h-6 peer-focus:animate-duration-200 peer-focus:animate-fade-out-up"></div>
         <IconsSearch
           color="white"
